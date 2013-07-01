7255ecc46df3e73b0834c222b361dc453fc90f43
alcopolis <myseconddigitalmail@yahoo.com>
2013-07-01 19:19:41 +0700

    Product frontpage design 60% complete

--- addons/shared_addons/modules/products/plugin.php
+++ addons/shared_addons/modules/products/plugin.php
@@ -23,6 +23,22 @@ class Plugin_Products extends Plugin
 	public function _self_doc()
 	{
 		$info = array(
+				'price' => array(
+						'description' => array(// a single sentence to explain the purpose of this method
+								'en' => 'Return Package detail',
+						),
+						'single' => true,// will it work as a single tag?
+						'double' => false,// how about as a double tag?
+						'variables' => '',// list all variables available inside the double tag. Separate them|like|this
+						'attributes' => array(
+								'group' => array(
+										'type' => 'text',// Can be: slug, number, flag, text, array, any.
+										'flags' => '',
+										'default' => '',
+										'required' => true,
+								),
+						),
+				),
 				'js' => array(
 						'description' => array(// a single sentence to explain the purpose of this method
 								'en' => 'Return custom JS',
@@ -63,7 +79,7 @@ class Plugin_Products extends Plugin
 	public function __construct()
 	{	
 		$this->load->model('products_m');
-	}
+	}	
 	
 
 	public function js()
@@ -74,6 +90,7 @@ class Plugin_Products extends Plugin
 		return '<script>' . $js . '</script>';
 	}
 	
+	
 	public function css()
 	{
 		$prod = $this->products_m->inn_get('product', $this->attribute('value'), 'slug', TRUE);
@@ -81,4 +98,42 @@ class Plugin_Products extends Plugin
 			
 		return '<style type="text/css">' . $css . '</style>';
 	}
+	
+	
+
+
+	public function price()
+	{
+		$return = '';
+		$packs = $this->products_m->inn_get_by('package', array('package_group'=>$this->attribute('group')));	
+	
+		//var_dump($packs);
+		
+		foreach($packs as $pack){
+			$prod_id = $pack->package_prod_id;
+			$pack_id = $pack->package_id;
+			
+			$default_field = $this->products_m->inn_get_by('field', array('product_id'=>$prod_id, 'package_id'=>0), TRUE);
+			$fields = $this->products_m->inn_get_by('field', array('product_id'=>$prod_id, 'package_id'=>$pack_id));
+			
+			//var_dump($fields);
+			
+			$return .= '<div style="float:left;margin:10px;" class="price-table" id="' . strtolower(implode('-', explode(' ', $pack->package_name))) . '">';
+			$return .= '<h2>' . $pack->package_name . '</h2>';
+			
+			foreach($fields as $field){
+				$return .= '<p><small>' . $field->field_name . '</small><br/>' . $field->field_value . '</p>';
+			}
+			
+			if($default_field != NULL){
+				$return .= '<p><small>' . $default_field->field_name . '</small><br/>' . $default_field->field_value . '</p>';
+			}
+			
+			$return .= '</div>';
+		}
+			
+		return $return;
+	}
+	
+	
 }
--- addons/shared_addons/themes/innovate/views/modules/products/products.php
+++ addons/shared_addons/themes/innovate/views/modules/products/products.php
@@ -24,10 +24,11 @@
 				
 		<!-- Begin contentWrapper -->
 		<div class="content-wrapper">
-			<div id="product-tags"><?php echo $product->data->product_tags; ?></div>					
+			<div id="product-tags" class="hide"><?php echo $product->data->product_tags; ?></div>					
 			<div id="product-body"><?php echo $product->data->product_body; ?></div>	
 			
-			<div id="packages" style="width:75%; margin:0 auto;">
+<?php /*	<div id="packages" style="width:75%; margin:0 auto;">
+				<?php $groups = array(); ?>
 				<?php if($product->packages != NULL) { ?>
 					<ul>
 					<?php foreach($product->packages as $package){ ?>
@@ -36,11 +37,64 @@
 							<p><?php echo $package->package_price; ?></p>
 						</div></li>
 					<?php } ?>
-					</ul>
+					</ul>			
+					
+					
+					<?php 
+						foreach($product->packages as $key=>$package){
+							if(!in_array($package->package_group, $groups)){
+									$groups[$key] = $package->package_group;
+							}
+						}
+						
+						foreach($groups as $group){
+							echo '<h1>' . $group . '</h1>'; ?>
+							{{ products:package group="<?php echo $group; ?>" }}
+					<?php } 
+						
+						//var_dump($groups);
+					?>
+					
 				<?php } ?>
 			</div>
+*/ ?>
+			<?php 
+				
+				//Cari semua grup yg dimiliki produk
+				$groups = array();
+				
+				foreach($product->packages as $package){
+					$temp = explode(' ', $package->package_group);
+					$name = strtolower(implode('-', $temp));
+					if(!in_array($name, $groups)){
+						$groups[$name] = $package->package_group;
+					}
+				}
 			
-			<div id="faq">
+			?>
+			<div id="packages" style="width:80%; margin:0 auto;">
+				<div class="tabs">
+					<ul class="tab-menu">
+						<?php foreach($groups as $key=>$value): ?>
+							<li><a href="#<?php echo $key; ?>"><?php echo $value; ?></a></li>
+						<?php endforeach; ?>
+					</ul>
+					
+					<?php foreach($groups as $key=>$value): ?>
+					<div class="tab-content" id="<?php echo $key ?>" style="clear:both;">
+						<div class="info pack">
+                    		<p>Paket <?php echo $value; ?> memberikan kemudahan untuk memilih kombinasi saluran favorit Anda. Daftar sekarang untuk menikmati layanan hiburan, berita dan olah raga dengan paket yang sangat fleksibel.</p>
+                    	</div>
+                    	
+                    	<div class="info price">
+                    		{{ products:price group="<?php echo $value; ?>" }}
+                    	</div>
+					</div>
+					<?php endforeach; ?>
+				</div>
+			</div>
+			
+			<div id="faq" style="clear:both;">
 				<h1>FAQ Goes Here</h1>
 			</div>
 		</div>
