b593bcc2635d67cbcc67adc7f3b457d7994291f7
alcopolis <myseconddigitalmail@yahoo.com>
2013-10-07 12:09:21 +0700

    refining css

--- addons/shared_addons/modules/epg/js/main.js
+++ addons/shared_addons/modules/epg/js/main.js
@@ -31,6 +31,8 @@ $(document).ready(function(){
 	
 	
 	
+	
+	
 	// ========================== TV Guide Function ========================//
 	
 	var t;
@@ -78,6 +80,40 @@ $(document).ready(function(){
 			t = setTimeout(hide, 10000);
 		})
 	})
+	
+	
+	// ==================================== EPG TABLE ==========================================//
+	
+	if($('#epg').length > 0){
+		$('.channel').each(function(){
+			var temp = $(this).html().truncate(10, false, 'right', '');
+			$(this).html(temp);
+		});
+		
+		$('.show div').each(function(){
+			var temp = $(this).html().truncate(15, false, 'right', '...');
+			$(this).html(temp);
+		});
+
+		
+		var epgContW = $('#tdata').width();
+		var epgContH = $('#tdata').height();
+
+		var blockDataWidth = 240;
+		var blockDataMargin = 3;
+
+		var showContW = (blockDataWidth + blockDataMargin) * 26;
+
+//		$('#tdata').width(epgContW + 16);
+		$('#tdata').height(epgContH + 16);
+		$('#tdata .sh-row').width(showContW);
+	}
+
+//	fnScroll = function(){
+//		console.log($('#theader').scrollLeft());
+//		$('#theader').scrollLeft($('#tdata').scrollLeft());
+//		$('#tcol').scrollTop($('#tdata').scrollTop());
+//	}
 })
 
 function hide(){
--- addons/shared_addons/themes/innovate/js/main.js
+++ addons/shared_addons/themes/innovate/js/main.js
@@ -28,41 +28,4 @@ $(document).ready(function() {
 			 bulletThumbs: false,
 			 fluid:true
 		});
-		
-		
-		
-		
-		// ==================================== EPG TABLE ==========================================//
-		
-		if($('#epg').length > 0){
-			$('.channel').each(function(){
-				var temp = $(this).html().truncate(10, false, 'right', '');
-				$(this).html(temp);
-			});
-			
-			$('.show div').each(function(){
-				var temp = $(this).html().truncate(15, false, 'right', '...');
-				$(this).html(temp);
-			});
-	
-			
-			var epgContW = $('#tdata').width();
-			var epgContH = $('#tdata').height();
-	
-			var blockDataWidth = 240;
-			var blockDataMargin = 3;
-	
-			var showContW = (blockDataWidth + blockDataMargin) * 26;
-	
-			$('#tdata').width(epgContW + 16);
-			$('#tdata').height(epgContH + 16);
-			$('#tdata .sh-row').width(showContW);
-		}
-
-//		fnScroll = function(){
-//			console.log($('#theader').scrollLeft());
-//			$('#theader').scrollLeft($('#tdata').scrollLeft());
-//			$('#tcol').scrollTop($('#tdata').scrollTop());
-//		}
-		
 });
