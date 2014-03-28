30ff4409fd8bd23730e7bfbce1406ad86ac2d82e
alcopolis <myseconddigitalmail@yahoo.com>
2014-03-28 17:17:19 +0700

    28 Mar 2014

--- addons/shared_addons/modules/faq/controllers/admin_groups.php
+++ addons/shared_addons/modules/faq/controllers/admin_groups.php
@@ -38,12 +38,15 @@ class Admin_Groups extends Admin_Controller
 		
 		
 		//Get Category Tree
-		$result = $this->db->where('parent_id', 0)->order_by('id', 'ASC')->get('default_inn_faq_category')->result();
+// 		$result = $this->db->where('parent_id', 0)->order_by('id', 'ASC')->get('default_inn_faq_category')->result();
 		
-		foreach($result as $cat){
-			echo '<h4>' . $cat->category . '</h4>';
-			$this->getTree($cat->id,0);
-		}
+// 		foreach($result as $cat){
+// 			echo '<h4>' . $cat->category . '</h4>';
+// 			$this->getTree($cat->id,0);
+// 		}
+
+		$this->cat_tree = $this->getTree();
+		
 	}
 	
 	
@@ -79,33 +82,66 @@ class Admin_Groups extends Admin_Controller
 // 				);
 
 		
-		$this->cat_tree = array(
-					'0' => array('level' => 0, 'parent' => NULL, 'slug' => '', 'cat' => 'No Parent'),
-					'1' => array('level' => 0, 'parent' => 0, 'slug' => 'other', 'cat' => 'Other'),
-					'2' => array('level' => 2, 'parent' => 11, 'slug' => 'billing', 'cat' => 'Billing'),
-					'6' => array('level' => 1, 'parent' => 11, 'slug' => 'mandiri-power-bills', 'cat' => 'Mandiri Power Bills'),
-					'11' => array('level' => 0, 'parent' => NULL, 'slug' => 'payment', 'cat' => 'Payment')
+		$temp = array(
+					0 => array('child'=>FALSE, 'level' => 0, 'parent' => NULL, 'slug' => '', 'cat' => 'No Parent'),
+					1 => array('child'=>FALSE, 'level' => 0, 'parent' => NULL, 'slug' => 'other', 'cat' => 'Other'),
+					2 => array('child'=>FALSE, 'level' => 1, 'parent' => 11, 'slug' => 'billing', 'cat' => 'Billing'),
+					6 => array('child'=>FALSE, 'level' => 1, 'parent' => 11, 'slug' => 'mandiri-power-bills', 'cat' => 'Mandiri Power Bills'),
+					11 => array('child'=>TRUE, 'level' => 0, 'parent' => NULL, 'slug' => 'payment', 'cat' => 'Payment')
 				);
 		
 		
 		
 		
+//		var_dump($tes);
 		
-		if($this->form_validation->run()){
-			$data = $this->alcopolis->array_from_post(array('category'), $this->input->post());
-			
-			//create slug
-			$tmp = strtolower($this->input->post('category'));
-			$data['slug'] = str_replace(' ', '-', $tmp);
-			
-			if($this->faq_cat_m->insert_category($data)){
-				redirect('admin/faq');
+		
+// 		$level = 0;
+// 		$spacer = '--';
+// 		foreach ($this->cat_tree as $id=>$branch){
+// 			if($branch['parent'] == NULL){
+// 				echo '<option value="' . $id . '">' . $branch['cat'] . '</option>';
+// 			}else{
+// 				echo '<option value="' . $id . '">' . str_repeat($spacer, $branch['level']) . '&nbsp;' . $branch['cat'] . '</option>';
+// 			}
+// 		}
+		
+		$level = 0;
+		$spacer = '--';
+		foreach ($this->cat_tree as $id=>$branch){
+			if($branch->parent_id == NULL){
+				echo $branch->category;
+			}else{
+				$parent = NULL;	
+				echo $spacer . $branch->category;
 			}
-		}else{
-			$this->cat_data = $this->faq_cat_m->add_new();
+			echo '</br>';
 		}
+		
+		
+		
+		
+//		var_dump($this->cat_tree);
+		
+		
+		
+		
+		
+// 		if($this->form_validation->run()){
+// 			$data = $this->alcopolis->array_from_post(array('category'), $this->input->post());
+			
+// 			//create slug
+// 			$tmp = strtolower($this->input->post('category'));
+// 			$data['slug'] = str_replace(' ', '-', $tmp);
+			
+// 			if($this->faq_cat_m->insert_category($data)){
+// 				redirect('admin/faq');
+// 			}
+// 		}else{
+// 			$this->cat_data = $this->faq_cat_m->add_new();
+// 		}
 					
-		$this->render('admin/category_form', array('cat'=>$this->cat_data, 'page'=>$this->page_data));
+// 		$this->render('admin/category_form', array('cat'=>$this->cat_data, 'page'=>$this->page_data));
 	}
 	
 	
@@ -116,14 +152,23 @@ class Admin_Groups extends Admin_Controller
 	}
 	
 	
-	private function getTree($parent, $level){
+	
+	protected $temp = array();
+	
+	private function getTree($parent = NULL, $level = 0){
 		$result = $this->db->where('parent_id', $parent)->get('default_inn_faq_category')->result();
 		
+		
 		foreach($result as $row){
-			echo $level > 0 ? '&nbsp;&nbsp;&raquo;' : '| ';
-			echo str_repeat('&nbsp;&nbsp;',$level) . $row->category . "</br>";
+			$this->temp[$row->id] = $row;
+			
 			$this->getTree($row->id, $level+1);
 		}
+		
+		//$tree = $this->arrangeCat($this->temp);
+		$tree = $this->temp;
+		
+		return $tree;
 	}
 	
 }
