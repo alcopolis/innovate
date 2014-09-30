9afd060e71ff06d84fc433754ac2af9256e7c452
alcopolis <myseconddigitalmail@yahoo.com>
2014-09-30 23:51:08 +0700

    Ganti routes

--- addons/shared_addons/modules/quiz/config/routes.php
+++
@@ -1,1 +0,0 @@
-<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');// front-end$route['quiz']						= 'index';$route['quiz(/:any)?']				= 'pages$1';$route['quiz/check/']		= 'check';// back-end$route['quiz/admin(/:any)?']				= 'admin/index$1';
