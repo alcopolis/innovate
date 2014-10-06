169d26498299919ee0efc1baa984234544138dff
alcopolis <myseconddigitalmail@yahoo.com>
2014-10-06 13:28:00 +0700

    Quiz 6 oktober

--- addons/shared_addons/themes/innovate/views/modules/quiz/index.php
+++ addons/shared_addons/themes/innovate/views/modules/quiz/index.php
@@ -23,7 +23,7 @@
 			<h1>Innovate Quiz</h1>				
 			<?php foreach($quiz as $q){ ?>
 				<div id="quiz-<?php echo $q->slug; ?>" class="item clearfix" style="position:relative; width:30%; background:url({{ url:site }}files/large/<?php echo $q->theme; ?>) no-repeat left top; background-size:100% auto; margin:10px 1.5%;float:left;">
-					<a href="quiz/pages/<?php echo $q->slug; ?>" class="button play-quiz left" style="position:relative; top:20px; padding:5px 10px; background:rgba(255,255,255,.95);color:#333; box-shadow:0 2px 3px #999; border:none;"><?php echo $q->name; ?></a>
+					<a href="quiz/pages/<?php echo $q->slug; ?>" class="button play-quiz left" style="position:relative; top:20px; padding:5px 10px; background:rgba(255,255,255,.95);color:#333; box-shadow:0 2px 3px #999; border:none; font-weight:bold;"><?php echo $q->name; ?></a>
 				</div>
 			<?php } ?>
 		</div>
--- addons/shared_addons/themes/innovate/views/modules/quiz/pages.php
+++ addons/shared_addons/themes/innovate/views/modules/quiz/pages.php
@@ -27,18 +27,27 @@
 		<div id="body-wrapper" class="clearfix">
 			<div class="container">
 				
+				<?php //var_dump($this->session->all_userdata()) ?>
+				<p>{{ session:flash name="quiz_msg" }}</p>
+				
 				
 				<section class="clearfix" id="quiz">
 					<h1 id="title"><?php echo $quiz->name; ?></h1>
 					
+					
 					{{if !user:logged_in }}
 						<div id="user-data">
-							<p>Silahkan login terlebih dahulu untuk mengikuti kuis ini</p>
+							<p>Silahkan login terlebih dahulu untuk mengikuti kuis ini dan menangkan tayangan <strong>Liga Inggris</strong> di <strong>InnovateGO</strong> GRATIS sampai akhir musim untuk 2 pemenang!</p>
 							{{ widgets:area slug="login" }}
 						</div>
 						<div id="popup" class="hide" >
 							<a id="close-popup" href="#" onclick="closePopup()">x</a>
 							<iframe src="register"></iframe>
+						</div>
+					{{ else }}
+						<div id="user-data">
+							<p>Menangkan tayangan Liga Inggris di <strong>InnovateGO GRATIS</strong> sampai akhir musim untuk 2 pemenang!</p>
+							{{ widgets:area slug="login" }}
 						</div>
 					{{ endif }}
 					
@@ -48,18 +57,17 @@
 					{{ else }}
 						<ol id="quiz-content" class="login-required">
 					{{ endif }}
-
 					
-					<?php echo form_open('quiz/check/' . $quiz->slug); ?>
-					<ol id="quiz-content">
 						<?php 
 							$i=0;
 						?>
-					
+						
 						<?php foreach($question as $quest){ ?>
 							<?php $soal = json_decode($quest->question_admin); $i++; ?>
+							
 							<li class="data">
 								<p><?php echo $soal->question; ?></p>
+								
 								<ol>
 									<?php foreach($soal->choices as $key=>$c){ ?>
 										<li><input value="<?php echo $key; ?>" name="q-<?php echo $i; ?>" type="radio" /> <?php echo $c; ?></li>
@@ -68,19 +76,25 @@
 							</li>
 						<?php } ?>
 						
-						<input type="hidden" name="total" value="<?php echo  count($question); ?>"/>
+						<input type="hidden" name="total" value="<?php echo count((array)$question); ?>"/>
+						
+						{{if user:logged_in}}
+							<input type="submit" value="Kirim Jawaban" name="submit">
+						{{ endif }}
 					</ol>
 					
-					<input type="submit" value="Kirim Jawaban" name="submit">
-					<?php echo form_close(); ?>
+					{{if user:logged_in}}
+						<?php echo form_close(); ?>
+					{{ endif }}
 				</section>
 				
 				<section id="rules" class="clearfix">
 					Syarat &amp; Ketentuan Kuis:
 					<ol>
+						<li>Periode Kuis 2 - 16 Oktober 2014</li>
 						<li>Kuis terbuka untuk umum</li>
 						<li>Pelanggan wajib mengisi semua data diri dan informasi yang dibutuhkan pada kolom yang tertera</li>
-						<li>Pemenang akan diumumkan melalui akun resmi twitter Innovate, @InnovateInd, paling lambat 7 hari setelah periode kuis berakhir</li>
+						<li>Pemenang akan diumumkan melalui akun resmi twitter Innovate, <strong>@InnovateInd</strong>, paling lambat 7 hari setelah periode kuis berakhir</li>
 						<li>Peserta dapat mengikuti kuis lebih dari 1 kali, tetapi hanya dapat terpilih 1 kali sebagai pemenang</li>
 						<li>Keputusan juri tidak dapat diganggu gugat</li>
 					</ol>
